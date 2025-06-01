074a381ac34dcc52b72179dd084a139f196f5497
David van Vliet <dv23@rice.edu>
2025-06-01 09:46:57 -0700

    config file added + getTargetUrl()

---
+++ ui-portal/src/config.js
@@ -0,0 +1,4 @@
+// API Configuration
+export const CONFIG = {
+  OPENAI_API_KEY: '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'
+}; 
