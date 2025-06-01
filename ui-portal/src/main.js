60cf940d8c47fc735ad7c1795772d94c942af215
Axel <axel.jeremy.7@gmail.com>
2025-05-31 17:50:42 -0700

    Update main.js
    fix to build

--- ui-portal/src/main.js
+++ ui-portal/src/main.js
@@ -1,13 +1,10 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
 
 document.querySelector('#app').innerHTML = `
   <div>
     <div class="header">
-      <a href="https://vite.dev" target="_blank">
-        <img src="${viteLogo}" class="logo" alt="Vite logo" />
-      </a>
+
       <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
         <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
       </a>
